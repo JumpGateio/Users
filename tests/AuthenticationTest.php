f9d7a2bbfcf6fe9424b508ae7ac343a97648f0b2
stygiansabyss <stygian.warlock.v2@gmail.com>
2017-03-29 10:17:44 -0500

    Moving test autoload to dev

--- tests/AuthenticationTest.php
+++ tests/AuthenticationTest.php
@@ -2,14 +2,27 @@
 
 namespace Tests;
 
+use Illuminate\View\View;
+use JumpGate\Users\Http\Controllers\Authentication;
+use JumpGate\Users\Http\Requests\Login;
+
 class AuthenticationTest extends TestCase
 {
     /** @test */
     public function it_displays_the_login_form()
     {
-        $result = app(\JumpGate\Users\Http\Controllers\Authentication::class)->index();
+        $result = app(Authentication::class)->index();
 
-        $this->assertInstanceOf(\Illuminate\View\View::class, $result);
+        $this->assertInstanceOf(View::class, $result);
+        $this->assertEquals('auth.login', $result->getName());
+    }
+
+    /** @test */
+    public function it_errors_when_an_incomplete_form_is_passed()
+    {
+        $result = app(Authentication::class)->handle(new Login);
+
+        $this->assertInstanceOf(View::class, $result);
         $this->assertEquals('auth.login', $result->getName());
     }
 }
