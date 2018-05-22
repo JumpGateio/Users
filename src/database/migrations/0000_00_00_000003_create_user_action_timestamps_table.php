8ee6e9b0a2f56919f9839036631123af9a959e58
stygiansabyss <stygian.warlock.v2@gmail.com>
2018-05-21 23:38:59 -0500

    Fixing class name for new migration

--- src/database/migrations/0000_00_00_000003_create_user_action_timestamps_table.php
+++ src/database/migrations/0000_00_00_000003_create_user_action_timestamps_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
-class CreateUserActionTimestampsTables extends Migration
+class CreateUserActionTimestampsTable extends Migration
 {
 
     /**
