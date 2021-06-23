6382723fcf52bf684a47f6fb072bdbbec2e0a583
Travis Blasingame <stygian.warlock.v2@gmail.com>
2021-06-23 11:55:33 -0500

    Create UserLoggedOut.php

---
+++ src/JumpGate/Users/Events/UserLoggedOut.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace JumpGate\Users\Events;
+
+use Illuminate\Queue\SerializesModels;
+use Laravel\Socialite\AbstractUser;
+use JumpGate\Users\Models\User;
+
+class UserLoggedOut
+{
+    use SerializesModels;
+
+    /**
+     * @var \JumpGate\Users\Models\User
+     */
+    public $user;
+
+    /**
+     * Create a new event instance.
+     *
+     * @param \JumpGate\Users\Models\User          $user
+     */
+    public function __construct(User $user)
+    {
+        $this->user       = $user;
+    }
+}
